d81bdb30069e09de60f2e09aa937b4aaa7b4ee21
Melvin Idema <melvin_i13@hotmail.com>
2023-04-20 15:32:58 +0200

    Remove emoji picker and smoothen emoji recommendation experience

--- chat-app/src/App-backups.js
+++ chat-app/src/App-backups.js
@@ -1,6 +1,7 @@
 import {useState, useEffect, useRef} from 'react';
 import {v4 as uuidv4} from 'uuid';
 import './App.css';
+import EmojiPicker from 'emoji-picker-react';
 import BeatLoader from "react-spinners/BeatLoader";
 
 function App() {
@@ -13,18 +14,24 @@ function App() {
 
     const [newMessage, setNewMessage] = useState('');
 
+    const [showEmojiPicker, setShowEmojiPicker] = useState(false);
     const [showLoader, setShowLoader] = useState(false);
 
     const [emojiRecommendationBarVisible, setEmojiRecommendationBarVisible] = useState(false);
-    const [emojiRecommendation, setEmojiRecommendation] = useState(null);
+    const [emojiRecommendation, setEmojiRecommendation] = useState({
+        "emoticon_pairs": [
+            ["👋", "😃"],
+            ["👋", "👀"],
+            ["🙌", "😊"],
+            ["😍", "👋"],
+            ["😃", "👀"],
+            ["😊", "🤝"]
+        ]
+    });
 
     const [emojiSelected, setEmojiSelected] = useState(false);
-    const [counter, setCounter] = useState(0);
 
     async function fetchEmojiRecommendation() {
-        setCounter(counter + 1);
-
-        console.log("fetching ", counter);
         try {
             const response = await fetch('https://serverless-shit.ikbenmel.vin/api/analyseEmoji', {
                 method: 'POST',
@@ -33,12 +40,20 @@ function App() {
                 }),
                 headers: {'Content-Type': 'application/json'}
             });
-            const res = await response.json();
-            return JSON.parse(res.data);
-        } catch (err) {
+            const json = await response.json();
+            return json;
+        } catch(err) {
             throw err;
         }
     }
+
+    useEffect(() => {
+        async function fetch() {
+            const result = await fetchEmojiRecommendation();
+            console.log(result);
+        }
+        fetch();
+    }, []);
 
     // TODO: Should be rewritten to when emojiRecommendation updates
     useEffect(() => {
@@ -54,15 +69,8 @@ function App() {
         const timer = setTimeout(async () => {
 
             if (newMessage.length > 0) {
-                try {
-                    const emojiRecommendationResponse = await fetchEmojiRecommendation();
-                    setEmojiRecommendation(emojiRecommendationResponse);
-                    setEmojiRecommendationBarVisible(true);
-                    setShowLoader(false);
-                } catch (err) {
-                    console.error(err);
-                    setShowLoader(false);
-                }
+                setShowLoader(false);
+                setEmojiRecommendationBarVisible(true);
             } else {
                 setShowLoader(false);
                 setEmojiRecommendationBarVisible(false);
@@ -82,22 +90,36 @@ function App() {
         }
         setMessages([...messages, newMessageObject]);
         setNewMessage('');
-        setEmojiRecommendationBarVisible(false);
+    }
+
+    function onEmojiClick(emojiData, event) {
+        setNewMessage(newMessage + emojiData.emoji);
+        setShowEmojiPicker(false);
+        setEmojiSelected(true);
+    }
+
+    function openEmojiPicker(e) {
+        e.preventDefault();
+        setShowEmojiPicker(!showEmojiPicker);
     }
 
     function onInput(e) {
         setNewMessage(e.target.value);
-        setEmojiSelected(false);
     }
 
     function onEmojiRecommendationClick(pair) {
         inputRef.current.focus();
-        setEmojiSelected(true);
         setNewMessage(newMessage + pair[0] + " " + pair[1]);
     }
 
     return (<>
+        {showEmojiPicker &&
+            <div className="picker">
+                <EmojiPicker onEmojiClick={onEmojiClick} lazyLoadEmojis={true}/>
+            </div>
+        }
         <div className="chat-container">
+
             <div className="messages-container">
                 <ul className="message-list">
                     {messages.map((message, index) => (
@@ -105,6 +127,7 @@ function App() {
                     ))}
                 </ul>
             </div>
+
 
             <div className="new-message-container">
                 {emojiRecommendationBarVisible &&
@@ -122,6 +145,7 @@ function App() {
 
                 {showLoader && <div className="emoji-recommendation-bar"><BeatLoader color="#6B8AFD"/></div>}
                 <form action="">
+                    <button onClick={openEmojiPicker}>😊</button>
                     <input ref={inputRef} type="text" onInput={onInput} value={newMessage}/>
                     <button onClick={onSubmit}>Send</button>
                 </form>
